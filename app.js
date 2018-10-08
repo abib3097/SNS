202c0ff1d18da2176366abae98d63e7a93bb1c4b
Abib Cortes <bibamcinstallers@gmail.com>
2018-10-08 17:14:16 -0400

    Second Commit

--- app.js
+++ app.js
@@ -6,22 +6,11 @@ const port = 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Scott is the man\n');
+  res.end('Hello World\n');
 });
 
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
 
-var mysql = require('mysql');
 
-var con = mysql.createConnection({
-  host: "localhost",
-  user: "myusername",
-  password: "mypassword"
-});
-
-con.connect(function(err) {
-  if (err) throw err;
-  console.log("Connected!");
-});
