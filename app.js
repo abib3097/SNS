e3f05b6c7faa336350a92b44c598e4007a32f7b3
kadror <bibamcinstallers@gmail.com>
2018-10-09 13:26:11 -0400

    Reseting to default

--- app.js
+++ app.js
@@ -1,24 +1,11 @@
 const http = require('http');
-
-const hostname = '127.0.0.1';
+ const hostname = '127.0.0.1';
 const port = 3000;
-
-const server = http.createServer((req, res) => {
+ const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-<<<<<<< HEAD
-  res.end('This is our SNS Home page Dror\n');
-=======
-  res.end('Hello World\n');
->>>>>>> 202c0ff1d18da2176366abae98d63e7a93bb1c4b
+  res.end('This is our SNS Home page\n');
 });
-
-server.listen(port, hostname, () => {
+ server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
-<<<<<<< HEAD
 });
-=======
-});
-
-
->>>>>>> 202c0ff1d18da2176366abae98d63e7a93bb1c4b
