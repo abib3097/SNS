37bf4d1c7b00390a087d430ad4592de17ef41647
yummyinmytummy <scott111794@gmail.com>
2018-10-09 13:48:43 -0400

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const port = 3000;
  const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('This is our SNS Home page\n');
+  res.end('This is Scotts SNS Home page\n');
 });
  server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
