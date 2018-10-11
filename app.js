e43e049de0e40b0a0b242b1600eb958ac4ca35fc
yummyinmytummy <scott111794@gmail.com>
2018-10-11 11:40:35 -0400

    Update app.js

--- app.js
+++ app.js
@@ -5,9 +5,9 @@ const port = 3000;
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
   res.end('This is Scotts SNS Home page\n');
-  <script async defer
-      src="https://maps.googleapis.com/maps/api/js?key=AIzaSyC-gKicseFd6TgJcT1IE3HixJqRxVaUsgA&callback=initMap">
-    </script>
+});
+const googleMapsClient = require('@google/maps').createClient({
+  key: 'AIzaSyC-gKicseFd6TgJcT1IE3HixJqRxVaUsgA'
 });
  server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
